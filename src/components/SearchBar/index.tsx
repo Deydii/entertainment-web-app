ddfbf48290c7e26879a6d58adde24fb55599ebb7
Audrey LOURI <audrey.louri@gmail.com>
2023-02-13 21:06:50 +0100

    fix form width

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -4,7 +4,7 @@ const SearchBar = () => {
   return(
     <div className='mt-[6.5vh] flex'>
       <img className="self-start" src={searchIcon} alt="search icon" />
-      <form className="ml-4 w-3/4	md:w-full">
+      <form className="ml-4 w-3/4	md:w-[90%]">
         <input
           className="w-full h-full pb-4 border-0 bg-transparent placeholder:text-base text-xl text-white md:placeholder:text-xl placeholder:opacity-60 cursor-pointer caret-red focus:outline-0 focus:border-b focus:border-white/50"
           type="text"
