efe9ce116b57947b838c3c47188a20ad460f37c7
Audrey LOURI <audrey.louri@gmail.com>
2023-02-13 20:31:15 +0100

    responsive navbar for tablet and mobile

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import Home from '../Home';
 
 const App = () => {
   return (
-    <div className="min-h-screen flex bg-blue-900">
+    <div className="min-h-screen flex flex-col bg-blue-900 lg:flex-row">
       <Navbar />
       <div className="ml-10 w-[90%]">
       <SearchBar />
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,10 +4,10 @@ import avatar from '../../assets/avatar/image-avatar.png';
 
 const Navbar = () => {
   return(
-    <nav className="flex flex-col items-center relative w-[7%] h-[960px] my-8 ml-8 pt-11 rounded-[20px] bg-blue-700">
-      <img src={logo} alt="logo" />
+    <nav className="flex justify-between items-center relative h-20 px-8 bg-blue-700 md:rounded-[20px] md:m-8 lg:flex-col lg:justify-start lg:w-[7%] lg:h-[960px] lg:ml-8 lg:pt-11 lg:px-0">
+      <img className="w-[25px] h-5 lg:w-auto lg:h-auto" src={logo} alt="logo" />
       <LinksMenu />
-      <img className="absolute bottom-9	w-10 h-10 mt-86 rounded-full border-white border-2" src={avatar} alt="avatar" />
+      <img className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 mt-86 rounded-full border-white border-2 lg:absolute lg:bottom-9" src={avatar} alt="avatar" />
     </nav>
   )
 };
