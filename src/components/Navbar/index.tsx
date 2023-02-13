e14382cc4726c598536adf0303daf1c0df7b7a23
Audrey LOURI <audrey.louri@gmail.com>
2023-02-13 18:18:02 +0100

    fix navbar height

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,7 +4,7 @@ import avatar from '../../assets/avatar/image-avatar.png';
 
 const Navbar = () => {
   return(
-    <nav className="flex flex-col items-center relative w-[7%] my-8 ml-8 pt-11 rounded-[20px] bg-blue-700">
+    <nav className="flex flex-col items-center relative w-[7%] h-[960px] my-8 ml-8 pt-11 rounded-[20px] bg-blue-700">
       <img src={logo} alt="logo" />
       <LinksMenu />
       <img className="absolute bottom-9	w-10 h-10 mt-86 rounded-full border-white border-2" src={avatar} alt="avatar" />
