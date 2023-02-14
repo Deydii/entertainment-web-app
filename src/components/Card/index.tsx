228852ed56e640b03b1d63167552ca370d5fb737
Audrey LOURI <audrey.louri@gmail.com>
2023-02-14 15:53:17 +0100

    responsive card for tablet and mobile

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,16 +1,26 @@
 import cardImage from '../../assets/thumbnails/the-great-lands/regular/large.jpg';
+import mediumCardImage from '../../assets/thumbnails/the-great-lands/regular/medium.jpg';
+import smallCardImage from '../../assets/thumbnails/the-great-lands/regular/small.jpg';
 import movieCategory from '../../assets/icons/icon-category-movie.svg';
 import playIcon from '../../assets/icons/icon-play.svg';
 
 const Card = () => {
   return (
     <>
-    <div className="relative w-72 h-auto">
+    <div className="relative w-[164px] md:w-[220px] lg:w-72 h-auto">
       <div className="relative group hover:cursor-pointer">
-        <img className="rounded-lg w-full h-[174px] group-hover:opacity-75" src={cardImage} alt="Beyond Earth movie" />
-        <button className="absolute opacity-0 group-hover:opacity-100 flex justify-center items-center space-x-4 z-2 w-28 h-12 rounded-full bg-white/[0.40] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2" type="button">
-          <img src={playIcon} alt="play icon" />
-          <p className="text-white text-lg">Play</p>
+        <div className="hidden lg:block">
+          <img className="rounded-lg w-full h-[174px] group-hover:opacity-75" src={cardImage} alt="Beyond Earth movie" />
+        </div>
+        <div className="hidden md:block lg:hidden">
+          <img className="rounded-lg w-full h-[140px] group-hover:opacity-75" src={mediumCardImage} alt="Beyond Earth movie" />
+        </div>
+        <div className="block md:hidden">
+          <img className="rounded-lg w-full h-[110px] group-hover:opacity-75" src={smallCardImage} alt="Beyond Earth movie" />
+        </div>
+        <button className="absolute opacity-0 group-hover:opacity-100 flex justify-center items-center space-x-4 z-2 w-20 h-6 md:w-24 md:h-8 lg:w-28 lg:h-12 rounded-full bg-white/[0.40] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2" type="button">
+          <img className="w-4 h-4 lg:w-auto lg:h-auto" src={playIcon} alt="play icon" />
+          <p className="text-white text-base lg:text-lg">Play</p>
         </button>
       </div>
       <div className="group flex justify-center items-center absolute z-2 top-3 right-4 rounded-full w-7 h-7 bg-blue-200 hover:bg-white hover:cursor-pointer">
@@ -19,7 +29,7 @@ const Card = () => {
         </svg>
       </div>
       <div className="mt-2">
-        <ul className="flex space-x-3 list-none text-xs text-white/75">
+        <ul className="flex space-x-2 lg:space-x-3 list-none text-xs text-white/75">
           <li>2019</li>
           <li className="flex items-center before:inline-block before:w-[3px] before:h-[3px] before:mr-2 before:bg-white/50 before:rounded-full">
             <img className="mr-2"src={movieCategory} alt="movie category icon" />
@@ -29,7 +39,7 @@ const Card = () => {
             PG
           </li>
         </ul>
-        <p className="flex space-x-3 list-none text-xl text-white">Beyond Earth</p>
+        <p className="mt-1 text-xs md:text-lg lg:text-xl text-white">Beyond Earth</p>
       </div>
     </div>
    </>
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -8,8 +8,8 @@ const Home = () => {
       <div className="mt-8 flex overflow-x-auto space-x-4 md:space-x-8">
         <TrendingCard />
       </div>
-      <h3 className="mt-8 text-2xl">Recommended for you</h3>
-      <div className="mt-8 mb-10 grid grid-cols-4 gap-x-1 gap-y-10">
+      <h3 className="mt-8 text-[20px] md:text-2xl">Recommended for you</h3>
+      <div className="mt-6 lg:mt-8 mb-10 grid min-[375px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 md:gap-x-12 lg:gap-x-1 gap-y-10">
         <Card />
       </div>
     </div>
