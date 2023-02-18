883c64d8f1a27a194fd4742446245d71dadd640e
Audrey LOURI <audrey.louri@gmail.com>
2023-02-18 12:21:13 +0100

    add react router and add create first route

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
 import { DataContextProvider } from './context/dataContext';
-import App from './components/App/App';
+import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />
+  }
+])
+
 root.render(
   <React.StrictMode>
     <DataContextProvider>
-      <App />
+    <div className="min-h-screen w-full overflow-hidden flex flex-col lg:flex-row">
+      <header className="w-full lg:w-32 lg:pl-8">
+        <Navbar />
+      </header>
+      <div className="ml-4 mb-14 md:ml-6 lg:ml-8 md:w-full lg:w-[90%]">
+        <SearchBar />
+        <RouterProvider router={router} />
+      </div>
+    </div>
     </DataContextProvider>
   </React.StrictMode>
 );
---
+++ src/routes/root.tsx
@@ -0,0 +1,9 @@
+import Home from '../components/Home';
+
+const Root = () => {
+  return (
+     <Home />
+  );
+};
+
+export default Root;
