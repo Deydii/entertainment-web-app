54a75c574568fb0569d9f51edcda2cc65cd2b175
Audrey LOURI <audrey.louri@gmail.com>
2023-02-18 19:18:13 +0100

    add filter in cards component

--- src/routes/cards.tsx
+++ src/routes/cards.tsx
@@ -9,12 +9,16 @@ interface NameCategory  {
 
 const Cards = ({ name }: NameCategory) => {
 
-  const { data } = useContext(DataContext);
+  const { data, show } = useContext(DataContext);
 
   const results:Results[] = data.filter(results => results.category.toLowerCase() === name);
 
+  const shows:Results[] = data.filter(shows => shows.title.toLowerCase().includes(show.toLowerCase()) && shows.category.toLowerCase() === name);
+
   return (
    <>
+   {!show && (
+    <>
     <h3 className="mt-4 text-white text-[20px] md:text-2xl">{name === "movie" ? "Movies" : "TV Series"}</h3>
     <div className="mt-6 mr-4 md:mr-6 lg:mr-8 lg:mt-8 grid grid-cols-1 gap-x-4 md:gap-x-7 lg:gap-x-10 gap-y-8 min-[375px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-[1700px]:grid-cols-5">
       {results.map(({ title, thumbnail, year, category, rating, isBookmarked }) => {
@@ -33,6 +37,29 @@ const Cards = ({ name }: NameCategory) => {
     }
    </div>
    </>
+   ) 
+  }
+  {show && (
+    <>
+      <h3 className="mt-4 text-[20px] text-white md:text-2xl">{`Found ${shows.length} results for ${show}`}</h3>
+      <div className="mr-4 md:mr-6 lg:mr-8 lg:mt-8 grid grid-cols-1 gap-x-4 md:gap-x-7 lg:gap-x-10 gap-y-8 min-[375px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-[1700px]:grid-cols-5">
+        {shows.map(({ title, thumbnail, year, category, rating, isBookmarked }) => {
+          return (
+            <Card 
+              key={title}
+              title={title}
+              thumbnail={thumbnail.regular}
+              year={year}
+              category={category}
+              rating={rating}
+              isBookmarked={isBookmarked}
+            />
+          )
+        })}
+      </div>
+    </>
+  )}
+  </>
   );
 };
 
