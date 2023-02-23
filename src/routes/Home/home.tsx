f3d5ee4bd7b60fed350fae61783c71ee62321c55
Audrey LOURI <audrey.louri@gmail.com>
2023-02-23 21:04:14 +0100

    add animation on cards

--- src/routes/Home/home.tsx
+++ src/routes/Home/home.tsx
@@ -1,10 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import { Transition } from '@headlessui/react';
 import { DataContext } from '../../context/dataContext';
 import TrendingCard from "./Trending";
 import Card from '../../components/Card';
 import { Results } from '../../interface/results';
 
+
 const Home = () => {
+
+  const [isShowing, setIsShowing] = useState(false)
 
   const { data, show } = useContext(DataContext);
 
@@ -12,42 +16,58 @@ const Home = () => {
 
   const results:Results[] = data.filter(shows => shows.title.toLowerCase().includes(show.toLowerCase()));
 
+  useEffect(() => setIsShowing(true), [])
+
   return (
     <div className="mt-4 text-white">
       {!show && (
         <>
           <h3 className="text-[20px] md:text-2xl">Trending</h3>
-          <div className="mt-8 pr-8 flex overflow-x-scroll overflow-y-hidden space-x-4 md:space-x-8">
-            {trending.map(({ title, thumbnail, year, category, rating }) => {
-              return (
-                <TrendingCard 
-                  key={title}
-                  title={title}
-                  thumbnail={thumbnail.trending}
-                  year={year}
-                  category={category}
-                  rating={rating}
-                />
-                )
-              })
-            }
-          </div>
+          <Transition 
+            show={isShowing}
+            enter="transition-opacity duration-700"
+            enterFrom="opacity-0"
+            enterTo='opacity-100'
+          >
+            <div className="mt-8 pr-8 flex overflow-x-scroll overflow-y-hidden space-x-4 md:space-x-8 transition duration-700 ease-in">
+              {trending.map(({ title, thumbnail, year, category, rating }) => {
+                return (
+                  <TrendingCard 
+                    key={title}
+                    title={title}
+                    thumbnail={thumbnail.trending}
+                    year={year}
+                    category={category}
+                    rating={rating}
+                  />
+                  )
+                })
+              }
+            </div>
+           </Transition>
           <h3 className="mt-8 text-[20px] md:text-2xl">Recommended for you</h3>
-          <div className="mt-6 mr-4 md:mr-6 lg:mr-8 lg:mt-8 grid grid-cols-1 gap-x-4 md:gap-x-7 lg:gap-x-10 gap-y-8 min-[375px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-[1700px]:grid-cols-5">
-            {data.map(({ title, thumbnail, year, category, rating, isBookmarked }) => {
-              return (
-                <Card 
-                  key={title}
-                  title={title}
-                  thumbnail={thumbnail.regular}
-                  year={year}
-                  category={category}
-                  rating={rating}
-                  isBookmarked={isBookmarked}
-                />
-              )
-            })}
-          </div>
+          <Transition 
+            show={isShowing} 
+            enter="transition-opacity duration-700"
+            enterFrom="opacity-0"
+            enterTo='opacity-100'
+          >
+            <div className="mt-6 mr-4 md:mr-6 lg:mr-8 lg:mt-8 grid grid-cols-1 gap-x-4 md:gap-x-7 lg:gap-x-10 gap-y-8 min-[375px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-[1700px]:grid-cols-5">
+              {data.map(({ title, thumbnail, year, category, rating, isBookmarked }) => {
+                return (
+                  <Card 
+                    key={title}
+                    title={title}
+                    thumbnail={thumbnail.regular}
+                    year={year}
+                    category={category}
+                    rating={rating}
+                    isBookmarked={isBookmarked}
+                  />
+                )
+              })}
+            </div>
+          </Transition>
         </>
         )
       }
