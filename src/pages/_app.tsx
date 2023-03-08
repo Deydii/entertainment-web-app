3bd5b5e05c6a8b51f068f48f402ce66a186dd83e
Audrey LOURI <audrey.louri@gmail.com>
2023-03-08 10:43:06 +0100

    add outfit font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Outfit } from 'next/font/google';
 import type { NextPage } from 'next';
 import { ReactElement, ReactNode } from 'react';
 import { DataContextProvider } from '../context/dataContext';
 import '../styles/globals.css';
+
+const outfit = Outfit({
+  weight: ["300", "500"],
+  subsets: ['latin']
+});
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -27,6 +33,14 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
         />
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
+      <>
+        <style jsx global>{`
+            html {
+              font-family: ${outfit.style.fontFamily};
+            }
+          `}
+        </style>
+      </>
       <DataContextProvider>
         {getLayout(<Component {...pageProps} />)}
       </DataContextProvider>
