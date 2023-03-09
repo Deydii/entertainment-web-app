41b4234933c8fa7c069824379b0c5e3125fe3cd8
Audrey LOURI <audrey.louri@gmail.com>
2023-03-09 19:45:40 +0100

    fix search bar component import

--- __tests__/SearchBar/SearchBar.test.tsx
+++ __tests__/SearchBar/SearchBar.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
-import SearchBar from "../../components/SearchBar";
+import SearchBar from "../../src/components/SearchBar";
 
 describe('Search bar component', () => {
 
