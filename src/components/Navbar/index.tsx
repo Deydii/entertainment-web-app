d6a92044e70c5e9e5c48b9a67e07056c2e687c7b
Audrey LOURI <audrey.louri@gmail.com>
2023-03-14 22:21:01 +0100

    add user icon in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -34,15 +34,11 @@ const Navbar = () => {
       </div>
       <LinksMenu />
       <div className="absolute right-2 space-x-2 md:right-8 flex items-center md:space-x-3 lg:flex-col lg:space-y-6 lg:space-x-0 lg:bottom-9">
-        <div className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10">
-          <Image 
-            className="mt-86 rounded-full border-white border-2" 
-            src="/images/avatar/image-avatar.png" 
-            width={100}
-            height={100}
-            alt="avatar" 
-          />
-        </div>
+        <svg className="w-6 h-6 md:w-8 md:h-8" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="24" height="24" fill="none"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M6 8C6 4.68629 8.68629 2 12 2C15.3137 2 18 4.68629 18 8C18 11.3137 15.3137 14 12 14C8.68629 14 6 11.3137 6 8Z" fill="#5A698F" />
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.43094 16.9025C7.05587 16.2213 9.2233 16 12 16C14.771 16 16.9351 16.2204 18.5586 16.8981C20.3012 17.6255 21.3708 18.8613 21.941 20.6587C22.1528 21.3267 21.6518 22 20.9592 22H3.03459C2.34482 22 1.84679 21.3297 2.0569 20.6654C2.62537 18.8681 3.69119 17.6318 5.43094 16.9025Z" fill="#5A698F"/>
+        </svg>
         <button
           type="button"
           onClick={handleOnClick}
