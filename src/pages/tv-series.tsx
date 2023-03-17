3b00960f416350f809e2c5375f58416797a7dbcd
Audrey LOURI <audrey.louri@gmail.com>
2023-03-17 15:10:55 +0100

    edit tv series page

--- src/pages/tv-series.tsx
+++ src/pages/tv-series.tsx
@@ -12,11 +12,11 @@ import { Results } from '../interface/results';
 
 const TvSeries: NextPageWithLayout = () => {
 
-  const { data, show } = useContext(DataContext);
+  const { popularShows, show } = useContext(DataContext);
 
-  const results:Results[] = data.filter(results => results.category.toLowerCase() === "tv series");
+  const results:Results[] = popularShows.filter(results => results.media === "tv");
 
-  const shows:Results[] = data.filter(shows => shows.title.toLowerCase().includes(show.toLowerCase()) && shows.category.toLowerCase() === "tv series");
+  //const shows:Results[] = data.filter(shows => shows.title.toLowerCase().includes(show.toLowerCase()) && shows.category.toLowerCase() === "tv series");
 
   return (
    <>
@@ -29,16 +29,17 @@ const TvSeries: NextPageWithLayout = () => {
       transition={{ ease: "easeOut", duration: 2 }}
     >
       <div className="mt-6 mr-4 md:mr-6 lg:mr-8 lg:mt-8 grid grid-cols-1 gap-x-4 md:gap-x-7 lg:gap-x-10 gap-y-8 min-[375px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-[1700px]:grid-cols-5">
-        {results.map(({ title, thumbnail, year, category, rating, isBookmarked }) => {
+        {results.map(({ id, name, title, first_air_date, release_date, backdrop_path, media }) => {
           return (
             <Card
-              key={title}
+              key={id}
+              id={id}
+              name={name}
               title={title}
-              thumbnail={thumbnail.regular}
-              year={year}
-              category={category}
-              rating={rating}
-              isBookmarked={isBookmarked}
+              first_air_date={first_air_date}
+              release_date={release_date}
+              backdrop_path={backdrop_path}
+              media={media}
             />
           )
         })} 
@@ -49,9 +50,9 @@ const TvSeries: NextPageWithLayout = () => {
   }
   {show && (
     <>
-      <h3 className="mt-4 text-[20px] text-white md:text-2xl">{`Found ${shows.length} results for ${show}`}</h3>
+      {/* <h3 className="mt-4 text-[20px] text-white md:text-2xl">{`Found ${shows.length} results for ${show}`}</h3> */}
         <div className="mr-4 md:mr-6 lg:mr-8 lg:mt-8 grid grid-cols-1 gap-x-4 md:gap-x-7 lg:gap-x-10 gap-y-8 min-[375px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-[1700px]:grid-cols-5">
-          {shows.map(({ title, thumbnail, year, category, rating, isBookmarked }) => {
+          {/* {shows.map(({ title, thumbnail, year, category, rating, isBookmarked }) => {
             return (
               <Card 
                 key={title}
@@ -63,7 +64,7 @@ const TvSeries: NextPageWithLayout = () => {
                 isBookmarked={isBookmarked}
               />
             )
-          })}
+          })} */}
         </div>
     </>
   )}
